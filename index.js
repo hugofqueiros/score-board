d3cb5d87f8ebdb0ebfd7d111d01b6825cb911653
Hugo Queirós <hugofqueiros@gmail.com>
2024-04-10 13:14:23 +0100

    feat: Match class implementation

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 class Match {
-    constructor() {
+    constructor(homeTeam, awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = 0;
+        this.awayScore = 0;
+        this.startTime = new Date();
+    }
 
+    updateScore(homeScore, awayScore) {
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
+    }
+
+    getTotalScore() {
+        return this.homeScore + this.awayScore;
     }
 }
 
 class Scoreboard {
     constructor() {
-        
+
     }
 }
